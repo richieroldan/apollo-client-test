1234bdbc7b406544dcdf96aa084fb431b9ed648c
Richie <richieroldan14@gmail.com>
2020-02-01 23:18:06 +0800

    Added Custom resolvers for isOptimistic Field

--- client/app/apollo/client.js
+++ client/app/apollo/client.js
@@ -1,16 +1,27 @@
-import { ApolloClient } from "@wora/apollo-offline";
-import { HttpLink } from "apollo-link-http";
+import { ApolloClient } from '@wora/apollo-offline';
+import { HttpLink } from 'apollo-link-http';
 import ApolloCache from '@wora/apollo-cache';
- 
+
 const httpLink = new HttpLink({
-  uri: "http://localhost:4000/graphql"
+  uri: 'http://localhost:4000/graphql',
 });
- 
+
 const client = new ApolloClient({
   link: httpLink,
   cache: new ApolloCache({
-    dataIdFromObject: o => o.id
-  })
+    dataIdFromObject: o => o.id,
+  }),
+  resolvers: {
+    Todo: {
+      isOptimistic: (obj, args, context) => {
+        // console.log('isOptimistic', obj, context.cache);
+        if (obj.isOptimistic) {
+          return true;
+        }
+        return false;
+      },
+    },
+  },
 });
 
-export default client
+export default client;
